d3e98b6e38d3a3ed021994502a0ad2d12f57a9b1
Timothy Hanke <tim.hanke@gmail.com>
2020-09-25 22:42:07 -0400

    error boundary drills

---
+++ src/playingwithdanger/Currency.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Currency extends Component {
+  render() {
+    const money = new Intl.NumberFormat(this.props.locale, {
+      style: "currency",
+      currency: this.props.currency,
+    }).format(this.props.value);
+    return <div className="currency">{money}</div>;
+  }
+}
