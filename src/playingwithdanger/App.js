63e843a7407cb90a7891860abf2b2d4c7c82bb4b
Timothy Hanke <tim.hanke@gmail.com>
2020-10-15 19:50:55 -0400

    render prop practice

--- src/playingwithdanger/App.js
+++ src/playingwithdanger/App.js
@@ -1,23 +1,62 @@
 import React, { Component } from "react";
-import Currency from "./Currency";
-import CurrencyError from "./CurrencyError";
+
+class Cat extends React.Component {
+  render() {
+    const mouse = this.props.mouse;
+    return (
+      // src\images\cat.png
+      <img
+        src="cat.gif"
+        style={{ position: "absolute", left: mouse.x, top: mouse.y }}
+        alt="cat"
+      />
+    );
+  }
+}
+
+class Mouse extends React.Component {
+  constructor(props) {
+    super(props);
+    // props.render
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ height: "100vh" }} onMouseMove={this.handleMouseMove}>
+        {/*
+          Instead of providing a static representation of what <Mouse> renders,
+          use the `render` prop to dynamically determine what to render.
+        */}
+        {this.props.render(this.state)}
+        {/* which evals to <Cat mouse={this.state} /> */}
+      </div>
+    );
+  }
+}
+
+class MouseTracker extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Move the mouse around!</h1>
+        <Mouse render={(mouse) => <Cat mouse={mouse} />} />
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Here are some currency values</h1>
-        <CurrencyError>
-          Germany: <Currency value={21} locale="de-DE" currency="US" />
-        </CurrencyError>
-        <CurrencyError>
-          USA: <Currency value={21} locale="en-US" currency="USD" />
-        </CurrencyError>
-        <h2>
-          The currency values displayed are subject to change without notice.
-        </h2>
-      </div>
-    );
+    return <MouseTracker />;
   }
 }
 
